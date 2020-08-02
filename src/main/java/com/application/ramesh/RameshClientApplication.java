9b10286e6cfd323424fd37338a80b984a41c6187
rameshchatty <chattyramesh412@gmail.com>
2020-08-02 12:43:01 +0530

    swagger enabled

--- src/main/java/com/application/ramesh/RameshClientApplication.java
+++ src/main/java/com/application/ramesh/RameshClientApplication.java
@@ -7,8 +7,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
+@EnableSwagger2
 public class RameshClientApplication {
 
 	public static void main(String[] args) {
@@ -24,5 +32,14 @@ public class RameshClientApplication {
 			}
 		};
 	}
+	
+	@Bean
+    public Docket api() {                
+        return new Docket(DocumentationType.SWAGGER_2)          
+          .select()
+          .apis(RequestHandlerSelectors.basePackage("com.application.ramesh"))
+          .paths(PathSelectors.any()	)
+          .build();
+    }
 
 }
